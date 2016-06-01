b2f34c356339a9138986a584fa8b9e130e2bf887
manumarca <manuel.marcatili@fdvsolutions.com>
2016-06-01 10:14:11 -0300

    Accounting Firms Added

--- AutomatedTesting/NukedEnviroment.cs
+++ AutomatedTesting/NukedEnviroment.cs
@@ -23,6 +23,11 @@ namespace AutomatedTesting
 {
     class NukedEnviroment
     {
+        #region Global Instances
+        PageObjectCaller poc = new PageObjectCaller();
+        UnitOfWork unitOfWork = new UnitOfWork();
+        #endregion
+
         [SetUp]
         public void Initialize()
         {
@@ -37,100 +42,98 @@ namespace AutomatedTesting
             //Logs In 
             login.Login(wantedEnviroment);
         }
-
-        #region Global Instances
-        PageObjectCaller poc = new PageObjectCaller();
-        UnitOfWork unitOfWork = new UnitOfWork();
-        #endregion
-
+        
         [Test]
         public void ExecuteTest()
         {
-            #region Navigate To Firm Memos
-            //Goes To Firm Memos Page
-            poc.HomePage.FirmMemosLink.WaitUntilClickable(WebDriver.Driver);
-            poc.HomePage.FirmMemosLink.Click();
-            //Waits for ajax loading to finish
-            BrowserActions.WaitForApplicationLoad(WebDriver.Driver);
-            #endregion
 
-            #region Opens Law Firm Pop Up
-            //Opens Law Firm Pop Up 
-            //poc.FirmMemosPage.LawFirmExpandFilter.WaitUntilClickable(WebDriver.Driver);
-            //poc.FirmMemosPage.LawFirmExpandFilter.Click();
-            #endregion
-            
-            #region Closes Law Firm Pop Up
-            //Closes Law Firm Pop Up
-            //poc.FirmMemosLawPopUp.LawFirmCancelFilter.WaitUntilClickable(WebDriver.Driver);
-            //poc.FirmMemosLawPopUp.LawFirmCancelFilter.Click();
-            #endregion
-            
-            #region Gets LawFirm Feed Names
-            //Can Use this 
-            var lawFirmFeed = unitOfWork.LawFirmFeed.GetList();
-            lawFirmFeed.RemoveAll(x => x.Name2 == "?");
-            //Or This 
 
-            #endregion
+            #region Old Code
+            //#region Navigate To Firm Memos
+            ////Goes To Firm Memos Page
+            //poc.HomePage.FirmMemosLink.WaitUntilClickable(WebDriver.Driver);
+            //poc.HomePage.FirmMemosLink.Click();
+            ////Waits for ajax loading to finish
+            //BrowserActions.WaitForApplicationLoad(WebDriver.Driver);
+            //#endregion
+
+            //#region Opens Law Firm Pop Up
+            ////Opens Law Firm Pop Up 
+            ////poc.FirmMemosPage.LawFirmExpandFilter.WaitUntilClickable(WebDriver.Driver);
+            ////poc.FirmMemosPage.LawFirmExpandFilter.Click();
+            //#endregion
+            
+            //#region Closes Law Firm Pop Up
+            ////Closes Law Firm Pop Up
+            ////poc.FirmMemosLawPopUp.LawFirmCancelFilter.WaitUntilClickable(WebDriver.Driver);
+            ////poc.FirmMemosLawPopUp.LawFirmCancelFilter.Click();
+            //#endregion
+            
+            //#region Gets LawFirm Feed Names
+            ////Can Use this 
+            //var lawFirmFeed = unitOfWork.LawFirmFeed.GetList();
+            //lawFirmFeed.RemoveAll(x => x.Name2 == "?");
+            ////Or This 
+
+            //#endregion
                         
-            //Takes each lawfirm to make the rssFeed
-            foreach (var lawFirm in lawFirmFeed)
-            {
-                #region Writes LawFirm
-                //Writes Law firm
-                poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(lawFirm.Name2);
-                BrowserActions.WaitForApplicationLoad(WebDriver.Driver);
-                //Enter to accept law fimr written
-                poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(OpenQA.Selenium.Keys.Tab);
-                #endregion
+            ////Takes each lawfirm to make the rssFeed
+            //foreach (var lawFirm in lawFirmFeed)
+            //{
+            //    #region Writes LawFirm
+            //    //Writes Law firm
+            //    poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(lawFirm.Name2);
+            //    BrowserActions.WaitForApplicationLoad(WebDriver.Driver);
+            //    //Enter to accept law fimr written
+            //    poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(OpenQA.Selenium.Keys.Tab);
+            //    #endregion
             
-                #region Searches
-                //Starts The search
-                poc.FirmMemosPage.SearchButton.WaitUntilClickable(WebDriver.Driver);
-                poc.FirmMemosPage.SearchButton.Click();
-                #endregion
+            //    #region Searches
+            //    //Starts The search
+            //    poc.FirmMemosPage.SearchButton.WaitUntilClickable(WebDriver.Driver);
+            //    poc.FirmMemosPage.SearchButton.Click();
+            //    #endregion
 
-                #region Opens AddAlert Pop Up
-                //Clicks to open AddAlertButton
-                bool clicked = false;
-                do
-                {
-                    poc.FirmMemosPage.AddAlertButton.Click();
-                    Thread.Sleep(1000);
-                    try { if (poc.FirmMemosAddAlertPopUp.CancelButton.Displayed) clicked = true; } catch { };  
-                }
-                while (!clicked);
-                #endregion
+            //    #region Opens AddAlert Pop Up
+            //    //Clicks to open AddAlertButton
+            //    bool clicked = false;
+            //    do
+            //    {
+            //        poc.FirmMemosPage.AddAlertButton.Click();
+            //        Thread.Sleep(1000);
+            //        try { if (poc.FirmMemosAddAlertPopUp.CancelButton.Displayed) clicked = true; } catch { };  
+            //    }
+            //    while (!clicked);
+            //    #endregion
 
-                //Id´s from Pop Up changing constantly looking workaround to capture the element with stronger and constant property
-                #region Id Bug
-                ////Clicks RSS Feed RadioButton
-                poc.FirmMemosAddAlertPopUp.RssFeedRadiobutton.WaitUntilClickable(WebDriver.Driver);
-                poc.FirmMemosAddAlertPopUp.RssFeedRadiobutton.Click();
-                ////Copy URL Link
-                unitOfWork.LawFirmFeed.UpdateObject(lawFirm.LawFirmName, "NewURL",poc.FirmMemosAddAlertPopUp.RSSFeedURLBox.GetAttribute("value")); 
-                ////Writes Down Alert Name
-                var alertName = "FM - " + lawFirm.Name2;
-                poc.FirmMemosAddAlertPopUp.AlertNameTextBox.SendKeys(alertName);
-                unitOfWork.LawFirmFeed.UpdateObject(lawFirm.LawFirmName,"AlertName",alertName);
-                #endregion Id Bug
+            //    //Id´s from Pop Up changing constantly looking workaround to capture the element with stronger and constant property
+            //    #region Id Bug
+            //    ////Clicks RSS Feed RadioButton
+            //    poc.FirmMemosAddAlertPopUp.RssFeedRadiobutton.WaitUntilClickable(WebDriver.Driver);
+            //    poc.FirmMemosAddAlertPopUp.RssFeedRadiobutton.Click();
+            //    ////Copy URL Link
+            //    unitOfWork.LawFirmFeed.UpdateObject(lawFirm.LawFirmName, "NewURL",poc.FirmMemosAddAlertPopUp.RSSFeedURLBox.GetAttribute("value")); 
+            //    ////Writes Down Alert Name
+            //    var alertName = "FM - " + lawFirm.Name2;
+            //    poc.FirmMemosAddAlertPopUp.AlertNameTextBox.SendKeys(alertName);
+            //    unitOfWork.LawFirmFeed.UpdateObject(lawFirm.LawFirmName,"AlertName",alertName);
+            //    #endregion Id Bug
                 
-                #region Cleans Up used Law Firm in Search Filter
-                //Exits Pop Up Canceling the operation
-                poc.FirmMemosAddAlertPopUp.CancelButton.Click();
-                poc.FirmMemosPage.CloseFirstTab.Click();
-                //Clears Existing Search
-                for (int i = 0; i <= 3; i++) poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(OpenQA.Selenium.Keys.Backspace);
-                #endregion
-            }
+            //    #region Cleans Up used Law Firm in Search Filter
+            //    //Exits Pop Up Canceling the operation
+            //    poc.FirmMemosAddAlertPopUp.CancelButton.Click();
+            //    poc.FirmMemosPage.CloseFirstTab.Click();
+            //    //Clears Existing Search
+            //    for (int i = 0; i <= 3; i++) poc.FirmMemosPage.LawFirmTextBoxFilter.SendKeys(OpenQA.Selenium.Keys.Backspace);
+            //    #endregion
+            //}
+            #endregion
+
         }
 
         [TearDown]
         public void TestCleanUp()
         {
-            //Just for this tests if fails inside of pop up may break CleanUp so if pop up open this will close it
-            //if (poc.FirmMemosAddAlertPopUp.CancelButton.Displayed) poc.FirmMemosAddAlertPopUp.CancelButton.Click();
             //Gets Logout Class
             LogoutActions logOut = new LogoutActions();
             //Logs Out from Application
