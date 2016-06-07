8aedaed32a3d9679c72b1eee17471dd1622fc8f9
manumarca <manuel.marcatili@fdvsolutions.com>
2016-06-07 09:34:51 -0300

    ttt

--- AutomatedTesting/TestCases/FirmMemos/AccountantFirms.cs
+++ AutomatedTesting/TestCases/FirmMemos/AccountantFirms.cs
@@ -34,7 +34,7 @@ namespace AutomatedTesting.TestConditions.FirmMemos
             var enviroment = unitOfWork.EnvironmentRepository.GetList();
             ModelsLibrary.Shared.GlobalSettings wantedEnviroment = enviroment.Find(x => x.Page == "FirmMemos");
             //Set which browser is going to run
-            BrowserActions.SetBrowser(wantedEnviroment);
+            BrowserActions.SetBrowser(wantedEnviroment.Browser);
             //Gets Login Class
             LoginActions login = new LoginActions();
             //Goes to Intelligize
