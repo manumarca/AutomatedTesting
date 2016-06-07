618892fed8aefd5568eeee838e35a5c66b212cc2
manumarca <manuel.marcatili@fdvsolutions.com>
2016-06-07 09:46:18 -0300

    Added Logger to solution

---
+++ Logger/FileManager/FileManager.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FManager
+{
+    public class FileManager
+    {
+        private static string _path;
+        private static string _fileName;
+
+        public FileManager (TestContext TestContext)
+        {
+            _path = @"C:/Logs/";
+            _fileName = String.Format("{0}-{1:mm-dd-yy}.txt", TestContext.Test.Name, DateTime.Now.Day); 
+        }
+
+
+        private bool FileExists ()
+        {
+            if (File.Exists(_path + _fileName))
+                return true;
+            else
+                return false;
+        }
+
+        public static void WriteLine (string msg)
+        {
+            using (StreamWriter sw = new StreamWriter(_path + _fileName))
+            {
+                sw.WriteLine(msg);
+            }
+        }
+    }
+}
---
+++ Logger/Log/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using FManager;
+using System.Diagnostics;
+
+namespace Log
+{
+    public class Logger
+    {
+        private static TestContext _context;
+        private static readonly Stopwatch _timer;
+        
+        public Logger (TestContext TestContext)
+        {
+            _context = TestContext;
+            Header();
+            FileManager file = new FileManager(_context);
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Sets the info that is going to be shown in the Visual Studio context
+        /// </summary>
+        public static void Info (string msg)
+        {
+            Console.WriteLine
+                (@"{0} - {1}"
+                ,_timer.Elapsed, msg);
+        }
+
+        /// <summary>
+        /// Sets the initial content of the Log file
+        /// </summary>
+        private static void Header ()
+        {
+            Console.WriteLine
+                (@"
+                {0} - Test Directory: {1}
+                ", _context.Test.Name, _context.TestDirectory);
+        }
+
+        /// <summary>
+        /// Sets the final content of the Log file
+        /// </summary>
+        private static void Footer ()
+        {
+            Console.WriteLine
+                (@"
+                    {0}
+                ", _context.Result);
+        }
+    }
+}
